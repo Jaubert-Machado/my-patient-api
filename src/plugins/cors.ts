ceaacfcef152e9cada83f8f575de87a9778246c1
Jaubert Machado <jaubert.machado@gmail.com>
2026-03-05 12:01:39 -0300

    feat: multiple cors

--- src/plugins/cors.ts
+++ src/plugins/cors.ts
@@ -4,7 +4,7 @@ import type { FastifyInstance } from 'fastify'
 
 export const corsPlugin = fp(async function (app: FastifyInstance) {
   await app.register(cors, {
-    origin: process.env.CORS_ORIGIN ?? 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN?.split(',').map(o => o.trim()) ?? ['http://localhost:3000'],
     credentials: true,
   })
 })
